e5069cb240489b088c9447e00cd793fa068ce8a0
Hoang Nguyen <hnguyen31@gmail.com>
2018-02-21 05:04:54 -0500

    implemented redux for calories inputed

--- react-client/src/components/CaloriesInputed.jsx
+++ react-client/src/components/CaloriesInputed.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import helpers from '../helpers.js';
+
+import { connect } from 'react-redux';
 
 const CaloriesInputed = (props) => {
     return (
         <span>
-            {props.todaysCalories === null ? 2000 : props.todaysCalories + "   =   "}
+            {console.log('calories inputed', props)}
+            {props.items === undefined ? 
+                2000 : 
+                helpers.calculateDailyCalories(props.items) + "   =   "}
+                 
         </span>
     )
 }
 
-export default CaloriesInputed;
+const mapStateToProps = (state) => {
+    const { stats } = state.getUserStats
+    const { email } = state.reducer
+    const { items } = state.todaysEntries
+    return { 
+        email,
+        items,
+        stats
+    }
+}
+
+export default connect (mapStateToProps, null)(CaloriesInputed);
