7baaed929dafdb9e1fc09546fa9f81e1c612a47d
Hoang Nguyen <hnguyen31@gmail.com>
2018-05-21 23:26:21 -0400

    debugging react router issues

--- react-client/src/components/Header.jsx
+++ react-client/src/components/Header.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { createStore } from 'redux';
-import AppBar from 'material-ui/AppBar';
-import { firebaseApp } from '../config/firebase.js';
-import { connect } from 'react-redux';
-import Popover from 'material-ui/Popover';
-import Menu from 'material-ui/Menu';
-import RaisedButton from 'material-ui/RaisedButton';
-import MenuItem from 'material-ui/MenuItem';
-import Drawer from 'material-ui/Drawer';
-import { logUser, logUserOUT } from '../actions/index.js';
-import reducer from '../reducers';
-import Toggle from 'material-ui/Toggle';
-
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { createStore } from "redux";
+import AppBar from "material-ui/AppBar";
+import { firebaseApp } from "../config/firebase.js";
+import { connect } from "react-redux";
+import Popover from "material-ui/Popover";
+import Menu from "material-ui/Menu";
+import RaisedButton from "material-ui/RaisedButton";
+import MenuItem from "material-ui/MenuItem";
+import Drawer from "material-ui/Drawer";
+import { logUser, logUserOUT } from "../actions/index.js";
+import reducer from "../reducers";
+import Toggle from "material-ui/Toggle";
 
 class Header extends React.Component {
   constructor(props) {
@@ -20,16 +19,14 @@ class Header extends React.Component {
     this.state = {
       open: false,
       error: {
-        message: ''
+        message: ""
       },
       signedIn: true
-    }
-    this.handleToggle = this.handleToggle.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-    this.handleRequestClose = this.handleRequestClose.bind(this)
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
   }
-
-
 
   handleClick(event) {
     // This prevents ghost click.
@@ -37,9 +34,9 @@ class Header extends React.Component {
 
     this.setState({
       open: true,
-      anchorEl: event.currentTarget,
+      anchorEl: event.currentTarget
     });
-  };
+  }
 
   handleToggle() {
     this.setState({ open: !this.state.open });
@@ -47,24 +44,25 @@ class Header extends React.Component {
 
   handleRequestClose() {
     this.setState({
-      open: false,
+      open: false
     });
-  };
+  }
 
   signOut() {
-    firebaseApp.auth().signOut()
-      .catch(function (error) {
+    firebaseApp
+      .auth()
+      .signOut()
+      .catch(function(error) {
         this.setState({
           error: error
-        })
+        });
       })
       .then(() => {
-        console.log(this.state)
+        console.log(this.state);
         this.setState({
           signedIn: false
-        })
-
-      })
+        });
+      });
   }
 
   render() {
@@ -75,49 +73,61 @@ class Header extends React.Component {
     let redirect;
     let dailySummary;
     if (!email) {
-      signUp = <MenuItem onClick={this.handleRequestClose}><Link to='/SignUp'>Sign Up</Link></MenuItem>
-      signIn = <MenuItem onClick={this.handleRequestClose}><Link to='/SignIn'>Login</Link></MenuItem>
+      signUp = (
+        <MenuItem onClick={this.handleRequestClose}>
+          <Link to="/SignUp">Sign Up</Link>
+        </MenuItem>
+      );
+      signIn = (
+        <MenuItem onClick={this.handleRequestClose}>
+          <Link to="/SignIn">Login</Link>
+        </MenuItem>
+      );
     } else {
-      signOut = <MenuItem onClick={() => this.signOut()}>SignOut</MenuItem>
-      dailySummary = <MenuItem onClick={this.handleRequestClose}><Link to='/Journal'>Journal</Link></MenuItem>
-      profile = <MenuItem onClick={this.handleRequestClose}><Link to='/Profile'>Profile</Link></MenuItem>
-
+      signOut = <MenuItem onClick={() => this.signOut()}>SignOut</MenuItem>;
+      dailySummary = (
+        <MenuItem onClick={this.handleRequestClose}>
+          <Link to="/Journal">Journal</Link>
+        </MenuItem>
+      );
+      profile = (
+        <MenuItem onClick={this.handleRequestClose}>
+          <Link to="/Profile">Profile</Link>
+        </MenuItem>
+      );
     }
 
-
-
     return (
-      <header >
-
-        <AppBar title="MacroTracker" onClick={this.handleToggle} >
+      <header>
+        <AppBar title="MacroTracker" onClick={this.handleToggle}>
           <Drawer
             docked={false}
             width={200}
             open={this.state.open}
-            onRequestChange={(open) => this.setState({ open })}
+            onRequestChange={open => this.setState({ open })}
           >
-            <MenuItem onClick={this.handleRequestClose}><Link to='/'>Home</Link></MenuItem>
+            <MenuItem onClick={this.handleRequestClose}>
+              <Link to="/">Home</Link>
+            </MenuItem>
             {signIn}
             {dailySummary}
             {profile}
             {signOut}
-            <MenuItem onClick={this.handleRequestClose}><Link to='/Create'>Create</Link></MenuItem>
-
+            <MenuItem onClick={this.handleRequestClose}>
+              <Link to="/Create">Create</Link>
+            </MenuItem>
           </Drawer>
-
         </AppBar>
-
       </header>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   const { email } = state.reducer;
   return {
     email
-  }
-}
-
+  };
+};
 
 export default connect(mapStateToProps, null)(Header);
