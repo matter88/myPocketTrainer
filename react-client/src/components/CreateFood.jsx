95066630d2ceba89ee9b29c89914cf9590e83d5d
Hoang Nguyen <hnguyen31@gmail.com>
2018-05-21 23:05:36 -0400

    building out skeleton of create food database

--- react-client/src/components/CreateFood.jsx
+++ react-client/src/components/CreateFood.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
+import TextField from 'material-ui/TextField';
 
 class CreateFood extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        }
-    }
-    
-
-    render () {
-        return(
-            <div>Hi From CreateFood
-            <h1> I love you so much lobster </h1>
-            
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <TextField
+          hintText="Hint Text"
+          floatingLabelText="Fixed Floating Label Text"
+          floatingLabelFixed={true}
+        />
+        
+        <br />
+      </div>
+    );
+  }
 }
 
 export default CreateFood;
