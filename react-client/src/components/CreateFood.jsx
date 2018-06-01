08daca6e852fc3b5dee1b53427e624c1ed962c5b
Hoang Nguyen <hnguyen31@gmail.com>
2018-05-31 23:49:45 -0400

    make it better

--- react-client/src/components/CreateFood.jsx
+++ react-client/src/components/CreateFood.jsx
@@ -13,7 +13,7 @@ class CreateFood extends React.Component {
     this.handleFoodName = this.handleFoodName.bind(this);
     this.handleCalories = this.handleCalories.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    console.log('test', 'test2')
+    console.log('test', 'test2', 'test3')
   }
 
   handleFoodName(event) {
