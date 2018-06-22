b9e4fc096979ae4c3ebd411ef560958ad8484ab4
Hoang Nguyen <hnguyen31@gmail.com>
2018-06-22 00:11:43 -0400

    made changes to models

--- models/user.js
+++ models/user.js
@@ -1,112 +1,126 @@
-var mongo = require('mongodb');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://test1:123@ds257485.mlab.com:57485/minimumviableproject', {useMongoClient:true});
+var mongo = require("mongodb");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://test1:123@ds257485.mlab.com:57485/minimumviableproject",
+  { useMongoClient: true }
+);
 // mongoose.connect('mongodb://localhost/loginapp')
 var db = mongoose.connection;
 
-db.on('error', function() {
-  console.log('mongoose connection error');
+db.on("error", function() {
+  console.log("mongoose connection error");
 });
 
-db.once('open', function() {
-  console.log('mongoose nutrients connected successfully');
+db.once("open", function() {
+  console.log("mongoose nutrients connected successfully");
 });
 
-var Schema = mongoose.Schema
+var Schema = mongoose.Schema;
 // const schema = mongoose.Schema;
 
 const userNutritionIntakeSchema = mongoose.Schema({
-    email: { type: String },
-    Name: { type: String },
-    createdAt: { type: Date },
-    Water: {type: Number, default: 0},
-    Calories: {type: Number, default: 0},
-    Protein: {type: Number, default: 0},
-    Fats: {type: Number, default: 0},
-    Carbohydrate: {type: Number, default: 0},
-    Fiber: {type: Number, default: 0},
-    Sugars: {type: Number, default: 0},
-    Calcium: {type: Number, default: 0},
-    Iron: {type: Number, default: 0},
-    Magenesium: {type: Number, default: 0},
-    Phophorous: {type: Number, default: 0},
-    Potassium: {type: Number, default: 0},
-    Sodium: {type: Number, default: 0},
-})
+  email: { type: String },
+  Name: { type: String },
+  createdAt: { type: Date },
+  Water: { type: Number, default: 0 },
+  Calories: { type: Number, default: 0 },
+  Protein: { type: Number, default: 0 },
+  Fats: { type: Number, default: 0 },
+  Carbohydrate: { type: Number, default: 0 },
+  Fiber: { type: Number, default: 0 },
+  Sugars: { type: Number, default: 0 },
+  Calcium: { type: Number, default: 0 },
+  Iron: { type: Number, default: 0 },
+  Magenesium: { type: Number, default: 0 },
+  Phophorous: { type: Number, default: 0 },
+  Potassium: { type: Number, default: 0 },
+  Sodium: { type: Number, default: 0 }
+});
 
-const userNutritionIntake = mongoose.model('userNutritionIntake', userNutritionIntakeSchema);
-
+const userNutritionIntake = mongoose.model(
+  "userNutritionIntake",
+  userNutritionIntakeSchema
+);
 
 var selectAll = () => {
-    return userNutritionIntake.find({}, function(err, entry) {
-      if(err) {
-        console.log(err);
-      } else {
-        console.log('success!');
-      }
-    });
-  };
+  return userNutritionIntake.find({}, function(err, entry) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("success!");
+    }
+  });
+};
 
-  var save = (nutrition) => {
-    console.log('serverside save invoked')
-    return userNutritionIntake.create(nutrition, function(err) {
-      if (err) {console.log(err)}
-      console.log('saved!!! BANANANANAN')
-    })
-  }
+var save = nutrition => {
+  console.log("serverside save invoked");
+  return userNutritionIntake.create(nutrition, function(err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("saved!!! BANANANANAN");
+  });
+};
 
 var todaysJournal = () => {
-  console.log('todays data invoked')
+  console.log("todays data invoked");
   let today = new Date();
-  today.setHours(0,0,0,0);
-  return userNutritionIntake.find({"createdAt" : { $gte : today }}, function(err, entry) {
-    if(err) {
+  today.setHours(0, 0, 0, 0);
+  return userNutritionIntake.find({ createdAt: { $gte: today } }, function(
+    err,
+    entry
+  ) {
+    if (err) {
       console.log(err);
     } else {
-      console.log('success pulled back todays journal!');
+      console.log("success pulled back todays journal!");
     }
   });
-}  
+};
 
 const yesterdayJournal = () => {
-  console.log('yesterday data invoked')
+  console.log("yesterday data invoked");
   let date = new Date();
-  date.setDate(date.getDate() - 1)
-  date.setHours(0,0,0,0);
-  return userNutritionIntake.find({"createdAt" : { $gte : date }}, function(err, entry) {
-    if(err) {
+  date.setDate(date.getDate() - 1);
+  date.setHours(0, 0, 0, 0);
+  return userNutritionIntake.find({ createdAt: { $gte: date } }, function(
+    err,
+    entry
+  ) {
+    if (err) {
       console.log(err);
     } else {
-      console.log('success pulled back yesterdays journal!');
+      console.log("success pulled back yesterdays journal!");
     }
   });
-} 
+};
 
 const tomorrowJournal = () => {
   let date = new Date();
-  date.setDate(date.getDate() + 1)
-  date.setHours(0,0,0,0);
-  return userNutritionIntake.find({"createdAt" : { $gte : date }}, function(err, entry) {
-    if(err) {
+  date.setDate(date.getDate() + 1);
+  date.setHours(0, 0, 0, 0);
+  return userNutritionIntake.find({ createdAt: { $gte: date } }, function(
+    err,
+    entry
+  ) {
+    if (err) {
       console.log(err);
     } else {
-      console.log('success pulled back tomorrows journal!')
+      console.log("success pulled back tomorrows journal!");
     }
   });
-}
+};
 
-const deleteEntry = (objId) => {
-  console.log('delete entry in model invoked')
+const deleteEntry = objId => {
+  console.log("delete entry in model invoked");
   return userNutritionIntake.remove({ _id: objId }, function(err) {
     if (err) {
-            console.log(err);
+      console.log(err);
+    } else {
+      console.log("successfully deleted");
     }
-    else {
-            console.log('successfully deleted');
-    }
-});
-}
-
+  });
+};
 
 exports.save = save;
 exports.selectAll = selectAll;
--- react-client/src/components/NdbnoResultsList.jsx
+++ react-client/src/components/NdbnoResultsList.jsx
@@ -1,103 +1,103 @@
-import React from 'react';
-import { MenuItem , ButtonToolbar, DropdownButton} from 'react-bootstrap';
-import store from '../reducers/store.js'
-import { setServingSize } from '../actions/index.js'
-import { connect } from 'react-redux'
-import { Table } from 'react-bootstrap'
-
+import React from "react";
+import { MenuItem, ButtonToolbar, DropdownButton } from "react-bootstrap";
+import store from "../reducers/store.js";
+import { setServingSize } from "../actions/index.js";
+import { connect } from "react-redux";
+import { Table } from "react-bootstrap";
 
 class NdbnoResultsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchInput: '',
+      searchInput: "",
       usdaList: [],
       usdaResults: [],
-      testState: '',
-      ndbno: '',
+      testState: "",
+      ndbno: "",
       nutrients: [],
-      itemName: "",
-    }
+      itemName: ""
+    };
     this.handleChange = this.handleChange.bind(this);
-    this.handleChangeSelect = this.handleChangeSelect.bind(this)
+    this.handleChangeSelect = this.handleChangeSelect.bind(this);
   }
 
   handleChange(event) {
-    store.dispatch(setServingSize(event.target.value))
+    store.dispatch(setServingSize(event.target.value));
   }
 
   handleChangeSelect(size) {
-    store.dispatch(setServingSize(size))
+    store.dispatch(setServingSize(size));
   }
 
- 
-
   render() {
-    let size = this.props.size
+    let size = this.props.size;
     let measuresArr = this.props.nutrients[0].measures;
     let loneMeasure = measuresArr[0].label;
     measuresArr.length === 1 ? this.handleChangeSelect(loneMeasure) : null;
     let temp = this.props.nutrients[0].measures;
     let labelIndex;
     if (temp) {
-        for (let i = 0; i < temp.length; i++) {
-            let sizeObj = temp[i];
+      for (let i = 0; i < temp.length; i++) {
+        let sizeObj = temp[i];
 
-            if (size === sizeObj.label) {
-                labelIndex = i;
-            }
+        if (size === sizeObj.label) {
+          labelIndex = i;
         }
+      }
     }
-    console.log('label', labelIndex)
+    console.log("label", labelIndex);
     return (
-
-      
-<div className="result-list">
-
-
-   <div className="table-nutrients">
-   <Table striped bordered condensed hover>
-<thead>
-
-
-    <th>Serving Size??</th>  
-    <select onChange={this.handleChange} >
-              {this.props.nutrients[0].measures.map((measure, index) =>
-                <option key={index} value={measure.label}>{measure.label}</option>)}
-          </select>
-</thead>
-<tbody>
-
-
-  {labelIndex !== undefined ?
-this.props.nutrients.map((nutrient, index) =>
-<tr className="table" key={index}>
-  <td>{nutrient.name === "Energy" ? "Calories" : nutrient.name}</td>
-  <td>{nutrient.measures[labelIndex].value + "  "}{nutrient.unit === "kcal" ? "calories" : nutrient.unit}</td>
-</tr>
-) :
-        <tr />
-
-        }
-</tbody>
-</Table>
-</div>
-</div>
-    )
+      <div className="result-list">
+        <div className="table-nutrients">
+          <Table striped bordered condensed hover>
+            <thead>
+              <th>Serving Size??</th>
+              <select onChange={this.handleChange}>
+                {this.props.nutrients[0].measures.map((measure, index) => (
+                  <option key={index} value={measure.label}>
+                    {measure.label}
+                  </option>
+                ))}
+              </select>
+            </thead>
+            <tbody>
+              {labelIndex !== undefined ? (
+                this.props.nutrients.map((nutrient, index) => (
+                  <tr className="table" key={index}>
+                    <td>
+                      {nutrient.name === "Energy" ? "Calories" : nutrient.name}
+                    </td>
+                    <td>
+                      {nutrient.measures[labelIndex].value + "  "}
+                      {nutrient.unit === "kcal" ? "calories" : nutrient.unit}
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr />
+              )}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  const { stats } = state.getUserStats
-  const { email } = state.reducer
-  const { items } = state.todaysEntries
-  const { size } = state.setServingSize
+const mapStateToProps = state => {
+  const { stats } = state.getUserStats;
+  const { email } = state.reducer;
+  const { items } = state.todaysEntries;
+  const { size } = state.setServingSize;
   return {
     email,
     items,
     stats,
     size
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, null)(NdbnoResultsList);
+export default connect(
+  mapStateToProps,
+  null
+)(NdbnoResultsList);
